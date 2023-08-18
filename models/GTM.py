6419824ed1e3bb84c21dece117fb7707972f9230
YusungAN <52990109+YusungAN@users.noreply.github.com>
2023-08-19 01:20:40 +0900

    Update GTM.py

--- models/GTM.py
+++ models/GTM.py
@@ -244,6 +244,8 @@ class GTM(pl.LightningModule):
         self.autoregressive = autoregressive
         self.gpu_num = gpu_num
         self.save_hyperparameters()
+        
+        self.validation_step_outputs = []
 
          # Encoder
         self.dummy_encoder = DummyEmbedder(embedding_dim)
@@ -307,6 +309,7 @@ class GTM(pl.LightningModule):
         item_sales, category, color, fabric, temporal_features, gtrends, images = train_batch 
         forecasted_sales, _ = self.forward(category, color, fabric, temporal_features, gtrends, images)
         loss = F.mse_loss(item_sales, forecasted_sales.squeeze())
+        self.validation_step_outputs.append(loss)
         self.log('train_loss', loss)
 
         return loss
@@ -317,8 +320,8 @@ class GTM(pl.LightningModule):
         
         return item_sales.squeeze(), forecasted_sales.squeeze()
 
-    def on_validation_epoch_end(self, val_step_outputs):
-        item_sales, forecasted_sales = [x[0] for x in val_step_outputs], [x[1] for x in val_step_outputs]
+    def on_validation_epoch_end(self):
+        item_sales, forecasted_sales = [x[0] for x in self.val_step_outputs], [x[1] for x in self.val_step_outputs]
         item_sales, forecasted_sales = torch.stack(item_sales), torch.stack(forecasted_sales)
         rescaled_item_sales, rescaled_forecasted_sales = item_sales*1065, forecasted_sales*1065 # 1065 is the normalization factor (max of the sales of the training set)
         loss = F.mse_loss(item_sales, forecasted_sales.squeeze())
