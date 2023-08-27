186c9c973bccc098dc4441e384414332b5af0760
YusungAN <52990109+YusungAN@users.noreply.github.com>
2023-08-28 04:48:17 +0900

    Update forecast.py

--- forecast.py
+++ forecast.py
@@ -34,20 +34,20 @@ def run(args):
     # Seeds for reproducibility
     pl.seed_everything(args.seed)
 
-    # Load sales data    
-    test_df = pd.read_csv(Path(args.data_folder + 'test.csv'), parse_dates=['release_date'])
-    item_codes = test_df['external_code'].values
+    # # Load sales data    
+    # test_df = pd.read_csv(Path(args.data_folder + 'test.csv'), parse_dates=['release_date'])
+    # item_codes = test_df['external_code'].values
 
-     # Load category and color encodings
-    cat_dict = torch.load(Path(args.data_folder + 'category_labels.pt'))
-    col_dict = torch.load(Path(args.data_folder + 'color_labels.pt'))
-    fab_dict = torch.load(Path(args.data_folder + 'fabric_labels.pt'))
+    #  # Load category and color encodings
+    # cat_dict = torch.load(Path(args.data_folder + 'category_labels.pt'))
+    # col_dict = torch.load(Path(args.data_folder + 'color_labels.pt'))
+    # fab_dict = torch.load(Path(args.data_folder + 'fabric_labels.pt'))
 
     # Load Google trends
-    gtrends = pd.read_csv(Path(args.data_folder + 'gtrends.csv'), index_col=[0], parse_dates=True)
+    # gtrends = pd.read_csv(Path(args.data_folder + 'gtrends.csv'), index_col=[0], parse_dates=True)
     
-    test_loader = ZeroShotDataset(test_df[:100], Path(args.data_folder + '/images'), gtrends, cat_dict, col_dict, \
-            fab_dict, args.trend_len).get_loader(batch_size=1, train=False)
+    # test_loader = ZeroShotDataset(test_df[:100], Path(args.data_folder + '/images'), gtrends, cat_dict, col_dict, \
+    #         fab_dict, args.trend_len).get_loader(batch_size=1, train=False)
 
 
     model_savename = f'{args.wandb_run}_{args.output_dim}'
@@ -95,19 +95,20 @@ def run(args):
     model.to(device)
     model.eval()
     gt, forecasts, attns = [], [],[]
-    for test_data in tqdm(test_loader, total=len(test_loader), ascii=True):
-        with torch.no_grad():
-            test_data = [tensor.to(device) for tensor in test_data]
-            item_sales, category, color, textures, temporal_features, gtrends =  test_data
-            y_pred, att = model(category, color,textures, temporal_features, gtrends)
-            forecasts.append(y_pred.detach().cpu().numpy().flatten()[:args.output_dim])
-            gt.append(item_sales.detach().cpu().numpy().flatten()[:args.output_dim])
-            attns.append(att.detach().cpu().numpy())
-
+    # for test_data in tqdm(test_loader, total=len(test_loader), ascii=True):
+    #     with torch.no_grad():
+    #         test_data = [tensor.to(device) for tensor in test_data]
+    #         item_sales, category, color, textures, temporal_features, gtrends =  test_data
+    #         y_pred, att = model(category, color,textures, temporal_features, gtrends)
+    #         forecasts.append(y_pred.detach().cpu().numpy().flatten()[:args.output_dim])
+    #         gt.append(item_sales.detach().cpu().numpy().flatten()[:args.output_dim])
+    #         attns.append(att.detach().cpu().numpy())
+    gtrends = [4.25469,4.41083,4.11319,3.20078,3.20078,3.33739,3.86435,3.73261,3.06416,3.5716,3.87899,3.48865,3.9717,3.66918,4.68894,4.43034,3.98633,3.62039,4.20102,3.64479,3.44962,2.7714,2.99585,3.77653,3.53744,3.33739,3.23005,3.1471,3.25933,3.07879,4.06928,37.72627,100,67.7775,41.59063,35.24762]
+    y_pred, att = model('다이어트 샐러드 건강 소스', gtrends)
     attns = np.stack(attns)
     forecasts = np.array(forecasts)
     gt = np.array(gt)
-
+    print(y_pred.detach().cpu().numpy().flatten()[:args.output_dim])
     rescale_vals = np.load(args.data_folder + 'stfore_sales_norm_scalar.npy')
     rescaled_forecasts = forecasts * rescale_vals
     rescaled_gt = gt * rescale_vals
@@ -131,8 +132,8 @@ if __name__ == '__main__':
     parser.add_argument('--use_trends', type=int, default=1)
     parser.add_argument('--use_img', type=int, default=0)
     parser.add_argument('--use_text', type=int, default=1)
-    parser.add_argument('--trend_len', type=int, default=52)
-    parser.add_argument('--num_trends', type=int, default=3)
+    parser.add_argument('--trend_len', type=int, default=36)
+    parser.add_argument('--num_trends', type=int, default=2)
     parser.add_argument('--embedding_dim', type=int, default=32)
     parser.add_argument('--hidden_dim', type=int, default=64)
     parser.add_argument('--output_dim', type=int, default=12)
