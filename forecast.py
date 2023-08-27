7b3685697610bd08784fe0ee8a4d2697eb6c13c2
YusungAN <52990109+YusungAN@users.noreply.github.com>
2023-08-28 05:26:27 +0900

    Update forecast.py

--- forecast.py
+++ forecast.py
@@ -46,8 +46,8 @@ def run(args):
 
     # Load Google trends
     # gtrends = pd.read_csv(Path(args.data_folder + 'gtrends.csv'), index_col=[0], parse_dates=True)
-    test_df = pd.read_csv('naver_searches_train2.csv').loc['랭킹닭컴닭가슴살']
-    test_loader = ZeroShotDataset(test_df, Path(args.data_folder + '/images'), gtrends, args.trend_len).get_loader(batch_size=1, train=False)
+    test_df = pd.read_csv('naver_searches_train2.csv')
+    test_loader = ZeroShotDataset(test_df[test_df['keyword'] == '랭킹닭컴닭가슴살'], Path(args.data_folder + '/images'), gtrends, args.trend_len).get_loader(batch_size=1, train=False)
 
 
     model_savename = f'{args.wandb_run}_{args.output_dim}'
