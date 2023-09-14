76ddba3c1f72dabd19b7b2396dfadc52ee681b40
YusungAN <52990109+YusungAN@users.noreply.github.com>
2023-09-14 12:34:40 +0900

    Update data_multitrends.py

--- utils/data_multitrends.py
+++ utils/data_multitrends.py
@@ -95,16 +95,19 @@ class ZeroShotDataset():
         res = 0
         words = []
         for k in range(len(data['keyword'].values)):
-            class_doc = reviews[reviews['keyword'] == k].values
-            df2 = pd.DataFrame(class_doc, columns=['text'])
-        
-            tfidf_vector = TfidfVectorizer()
-            tfidf_matrix = tfidf_vector.fit_transform(df2['text']).toarray()
-            tfidf_feature = tfidf_vector.get_feature_names_out()
-            result = pd.DataFrame(tfidf_matrix, columns=tfidf_feature)
-            tmp = result.iloc[k]
-            tmp.sort_values(ascending=False, inplace=True)
-            words.append(' '.join(list(tmp[:5].index)+[k, data[data['keyword'] == k]['cat2']]))
+            class_doc = reviews[reviews['keyword'] == k]
+            if len(class_doc.index) != 0:
+                df2 = pd.DataFrame(class_doc.values, columns=['text'])
+            
+                tfidf_vector = TfidfVectorizer()
+                tfidf_matrix = tfidf_vector.fit_transform(df2['text']).toarray()
+                tfidf_feature = tfidf_vector.get_feature_names_out()
+                result = pd.DataFrame(tfidf_matrix, columns=tfidf_feature)
+                tmp = result.iloc[k]
+                tmp.sort_values(ascending=False, inplace=True)
+                words.append(' '.join(list(tmp[:5].index)+[k, data[data['keyword'] == k]['cat2']]))
+            else:
+                words.append([k, data[data['keyword'] == k]['cat2'], data[data['keyword'] == k]['cat3']]))
         word_embeddings = model.encode(words)
         text = torch.FloatTensor(word_embeddings)
         # images = torch.stack(image_features)
