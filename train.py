47c27a22e893a09cfc0f00c8743ad72552bb8271
YusungAN <52990109+YusungAN@users.noreply.github.com>
2023-09-24 11:08:22 +0900

    Update train.py

--- train.py
+++ train.py
@@ -40,12 +40,12 @@ def run(args):
     train_df.info()
     train_df = train_df.sample(frac=1)
     print(train_df['keyword'].head(5))
-    train_loader = ZeroShotDataset(train_df[:24017], Path(args.data_folder + '/images'), gtrends, args.trend_len, reviews_df).get_loader(batch_size=args.batch_size, train=True)
-    test_loader = ZeroShotDataset(train_df[24017:], Path(args.data_folder + '/images'), gtrends, args.trend_len, reviews_df).get_loader(batch_size=args.batch_size, train=True)
-    with open("train_loader.pickle","wb") as fw:
-        pickle.dump(train_loader, fw)
-    with open("test_loader.pickle","wb") as fw:
-        pickle.dump(test_loader, fw)
+    # train_loader = ZeroShotDataset(train_df[:24017], Path(args.data_folder + '/images'), gtrends, args.trend_len, reviews_df).get_loader(batch_size=args.batch_size, train=True)
+    # test_loader = ZeroShotDataset(train_df[24017:], Path(args.data_folder + '/images'), gtrends, args.trend_len, reviews_df).get_loader(batch_size=args.batch_size, train=True)
+    with open("train_loader.pickle","rb") as fw:
+        train_loader = pickle.load(fw)
+    with open("test_loader.pickle","rb") as fw:
+        test_loader = pickle.load(fw)
     
     # Create model
     if args.model_type == 'FCN':
